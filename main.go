4d4fdbdcf0ff7d18f89179a0415e64d011f1907c
Aergiaaa <muzakren45@gmail.com>
2026-02-17 21:24:07 +0700

    feat: trying adding compiling mode to my idiot language (using VM)

--- main.go
+++ main.go
@@ -14,48 +14,61 @@ import (
 )
 
 func main() {
+
 	if len(os.Args) >= 3 && os.Args[1] == "run" {
-		filename := os.Args[2]
-
-		// check the extension
-		if filepath.Ext(filename) != ".simp" {
-			fmt.Fprintf(os.Stderr, "Error file must have .simp extension\n")
-			os.Exit(1)
-		}
-
-		// exec file mode
-		content, err := os.ReadFile(filename)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-			os.Exit(1)
-		}
-		input := string(content)
-
-		env := object.InitEnv()
-		l := lexer.Init(input)
-		p := parser.Init(l)
-		program := p.Parse()
-
-		if len(p.Errors()) != 0 {
-			for _, err := range p.Errors() {
-				fmt.Fprintf(os.Stderr, "Parser Error: %s\n", err)
-			}
-			os.Exit(1)
-		}
-
-		result := evaluator.Eval(program, env)
-		if result != nil && result.Type() == object.ERR_OBJ {
-			fmt.Fprintf(os.Stderr, "%s\n", result.Inspect())
-			os.Exit(1)
-		}
-		return
+		readFile()
 	}
-
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
 
+	var mode repl.COMPILE_MODE = repl.COMPILE
+	if os.Args[1] == "i" || os.Args[1] == "c" {
+		switch os.Args[1] {
+		case "i":
+			mode = repl.INTERPRET
+		case "c":
+			mode = repl.COMPILE
+		}
+	}
+
 	fmt.Printf("Hello %s! welcome to idiotic stupid language!\n", user.Username)
-	repl.Start(os.Stdin, os.Stdout)
+	repl.Start(os.Stdin, os.Stdout, mode)
+}
+
+func readFile() {
+	filename := os.Args[2]
+
+	// check the extension
+	if filepath.Ext(filename) != ".simp" {
+		fmt.Fprintf(os.Stderr, "Error file must have .simp extension\n")
+		os.Exit(1)
+	}
+
+	// exec file mode
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+	input := string(content)
+
+	env := object.InitEnv()
+	l := lexer.Init(input)
+	p := parser.Init(l)
+	program := p.Parse()
+
+	if len(p.Errors()) != 0 {
+		for _, err := range p.Errors() {
+			fmt.Fprintf(os.Stderr, "Parser Error: %s\n", err)
+		}
+		os.Exit(1)
+	}
+
+	result := evaluator.Eval(program, env)
+	if result != nil && result.Type() == object.ERR_OBJ {
+		fmt.Fprintf(os.Stderr, "%s\n", result.Inspect())
+		os.Exit(1)
+	}
 }
